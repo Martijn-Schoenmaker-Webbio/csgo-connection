cf1f388beb5d293bf8a637d1edc4bc688d6e75c3
Martijn Schoenmaker Webbio <martijn@webbio.nl>
2019-09-20 13:33:55 +0200

    fix: added matches

--- src/server/db/matches.ts
+++ src/server/db/matches.ts
@@ -2,6 +2,17 @@ import { Connection } from "./index";
 
 export const all = async () => {
   return new Promise((resolve, reject) => {
+    Connection.query(mostRecentMatchDetailed(3), (err, results) => {
+      if (err) {
+        return reject(err);
+      }
+      resolve(results);
+    });
+  });
+};
+
+export const all2 = async () => {
+  return new Promise((resolve, reject) => {
     Connection.query(mostRecentMatch, (err, results) => {
       if (err) {
         return reject(err);
@@ -19,3 +30,15 @@ const mostRecentMatch = `SELECT *
 FROM matches
 WHERE isValidMatch = 1
 ORDER BY id DESC LIMIT 1`;
+
+const mostRecentMatchDetailed = amountOfMatches => `SELECT * 
+FROM allValidMatches 
+WHERE
+(matchId >= 
+  (SELECT id
+  FROM
+  matches c
+  WHERE
+  c.isValidMatch = 1
+  ORDER BY id DESC
+  LIMIT ${amountOfMatches - 1}, 1))`;
